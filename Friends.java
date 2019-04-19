ad56c5e04e8d281395747d0157207b2bf427e323
woochanleee <030219woo@naver.com>
2019-04-19 23:56:14 +0900

    Parents class

---
+++ Friends.java
@@ -0,0 +1,13 @@
+public class Friends {
+    String name;
+    int age;
+    int height;
+
+    public void getName(String name) {
+        this.name = name;
+    }
+    public void getAge(int age) { this.age = age; }
+    public void getHeight(int height) { this.height = height; }
+}
+
+
